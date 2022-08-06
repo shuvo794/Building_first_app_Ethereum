00edc6f79e110fb2c6dd3c8d619df27a288bc269
shuvo794 <shuvohossain794@gmail.com>
2022-08-06 11:02:22 +0600

    sol versin update

--- contracts/HelloWorld.sol
+++ contracts/HelloWorld.sol
@@ -1,6 +1,6 @@
-pragma solidity >=0.5.0;
+pragma solidity >=0.8.15;
 contract HelloWorld{
-    string private helloMassge="Hello World"
+    string private helloMassge="Hello World";
     function getHelloMassge() public view returns(string memory){
         return helloMassge;
     }
